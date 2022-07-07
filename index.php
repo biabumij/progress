c330a7af3e4e07f03e88b043e4821c322c1f0f75
ginanjarbayu92@gmail.com <ginanjarbayu92@gmail.com>
2022-07-07 15:09:47 +0700

    update

--- index.php
+++ index.php
@@ -205,15 +205,16 @@
 		graphv[0] = ['&bull; SC & TEMEF - Perbaikan Query SQL Lap. Sales Order, Pesanan Pembelian, Daftar Tagihan', 80];
 		graphv[1] = ['&bull; TEMEF - RAP', 15];
 		graphv[2] = ['&bull; TEMEF - Pembuatan Menu Pergerakan Bahan Jadi (Stok) & Evaluasi', 80,];
-		graphv[3] = ['&bull; SC & TEMEF - Perbaikan Query SQL Laporan Hutang & Piutang', 100];
-		graphv[4] = ['&bull; SC & TEMEF - Dashboard Laba Rugi', 100,];
-		graphv[5] = ['&bull; TEMEF - Revisi TTD TEMEF (*END)', 100,];
-		graphv[6] = ['&bull; SC - Revisi TTD SC (*RIF)', 100];
-		graphv[7] = ['&bull; SC - Revisi Pergerakan Bahan Jadi SC, Jika Tidak Ada Produksi', 100,];
-		graphv[8] = ['&bull; TEMEF - Perbaikan Judul Pemakaian Alat & Output Biaya (*END)', 100,];
-		graphv[9] = ['&bull; TEMEF - Perbaikan Judul Biaya & TTD (*DEB)', 100,];
-		graphv[10] = ['&bull; SC - Import Database SC to jurnal.id (*DEB)', 100];
-		graphv[11] = ['&bull; SC & TEMEF - Tanda Warning jika (Minus)', 100];
+		graphv[3] = ['&bull; SC - Pembuatan Dashboard Harga Jual - Bahan Jadi', 100];
+		graphv[4] = ['&bull; SC & TEMEF - Perbaikan Query SQL Laporan Hutang & Piutang', 100];
+		graphv[5] = ['&bull; SC & TEMEF - Dashboard Laba Rugi', 100,];
+		graphv[6] = ['&bull; TEMEF - Revisi TTD TEMEF (*END)', 100,];
+		graphv[7] = ['&bull; SC - Revisi TTD SC (*RIF)', 100];
+		graphv[8] = ['&bull; SC - Revisi Pergerakan Bahan Jadi SC, Jika Tidak Ada Produksi', 100,];
+		graphv[9] = ['&bull; TEMEF - Perbaikan Judul Pemakaian Alat & Output Biaya (*END)', 100,];
+		graphv[10] = ['&bull; TEMEF - Perbaikan Judul Biaya & TTD (*DEB)', 100,];
+		graphv[11] = ['&bull; SC - Import Database SC to jurnal.id (*DEB)', 100];
+		graphv[12] = ['&bull; SC & TEMEF - Tanda Warning jika (Minus)', 100];
 
 		
 
